15feeb4ab5206afc0c23eb96ac546c016ffefdf1
Hyun-je <bigae2@gmail.com>
2024-08-04 16:30:05 +0900

    Add required Python dependencies to requirements.txt

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y update
+sudo apt-get -y install git python3-pip liblo-dev
+
+sudo apt remove python3-rpi.gpio
+sudo apt install python3-rpi-lgpio
+
+pip install -r requirements.txt --break-system-packages
