be2da5d63ce22e08b046edc4395d670b486ed82e
Hyun-je <bigae2@gmail.com>
2024-09-01 16:26:49 +0900

    chore: Update dependencies and add new Python modules to requirements.txt

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 sudo apt-get -y update
 sudo apt-get -y install git python3-pip liblo-dev spidev i2c-tools
+sudo apt-get -y install libopenblas-dev # for numpy
+sudo apt-get -y install libopenjp2-7 # for PIL
 
 sudo apt remove python3-rpi.gpio
 sudo apt install python3-rpi-lgpio
 
 pip install -r requirements.txt --break-system-packages
+
+# Setup I2C
+sudo raspi-config nonint do_i2c 0
+sudo i2cdetect -y 1
