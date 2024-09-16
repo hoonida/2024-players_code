5b402dcc75b7ac1a85b520d84b3a365b9aa149f1
Hyun-je <bigae2@gmail.com>
2024-09-16 16:51:03 +0900

    Refactor oled.py to include IP address on OLED display

--- service/oled/oled.py
+++ service/oled/oled.py
@@ -13,8 +13,9 @@ def get_ip():
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
+    ip_text = s.getsockname()[0]
     s.close()
-    return s.getsockname()[0]
+    return ip_text
 
 
 def main():
