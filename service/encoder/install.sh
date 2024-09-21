e5652cc2b61fc1dc96738656670f592069ee4d89
Hyun-je <bigae2@gmail.com>
2024-09-21 21:45:26 +0900

    Add systemd service installation scripts for ADC and encoder

---
+++ service/encoder/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USER_SERVICE_DIR=/etc/systemd/system
+SERVICE_FILE=encoder.service
+
+mkdir $USER_SERVICE_DIR
+
+echo "Create service file : $SERVICE_FILE"
+printf "\
+[Unit]
+After=network.target sound.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python /home/pi/2024-players_code/service/encoder/encoder.py
+User=pi
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=default.target
+" > $USER_SERVICE_DIR/$SERVICE_FILE
+
+chmod +x $USER_SERVICE_DIR/$SERVICE_FILE
+
+echo "Start service : $SERVICE_FILE"
+systemctl daemon-reload
+systemctl reenable $SERVICE_FILE
+systemctl restart $SERVICE_FILE
+systemctl status $SERVICE_FILE
+
